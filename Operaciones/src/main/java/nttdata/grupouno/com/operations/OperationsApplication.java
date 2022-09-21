3b2e1b31ffdc7abee2b1c6005e6fc34749e0a24a
jcastillo20 <juancrcastillo20@gmail.com>
2022-09-21 16:26:33 -0500

    Eureka Server

--- Operaciones/src/main/java/nttdata/grupouno/com/operations/OperationsApplication.java
+++ Operaciones/src/main/java/nttdata/grupouno/com/operations/OperationsApplication.java
@@ -2,7 +2,9 @@ package nttdata.grupouno.com.operations;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class OperationsApplication {
 
