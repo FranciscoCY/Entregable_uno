79140ff6219ae8d92c2e08c77f24d92c11764276
jcastillo20 <juancrcastillo20@gmail.com>
2022-09-22 12:31:15 -0500

    Cambios

--- registry-service/registry-service/src/main/java/nttdata/grupouno/com/registryservice/RegistryServiceApplication.java
+++ registry-service/registry-service/src/main/java/nttdata/grupouno/com/registryservice/RegistryServiceApplication.java
@@ -1,6 +1,5 @@
 package nttdata.grupouno.com.registryservice;
 
-import jdk.jfr.Enabled;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
