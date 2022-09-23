a75a9169c7120a50f4019270b8ba78b7b91d07ac
Francisco Frank CY <108429033+FranciscoCY@users.noreply.github.com>
2022-09-23 17:01:32 -0500

    Postman actualizado

--- Operaciones/src/test/java/nttdata/grupouno/com/operations/controllers/MasterAccountControllerTest.java
+++ Operaciones/src/test/java/nttdata/grupouno/com/operations/controllers/MasterAccountControllerTest.java
@@ -1,25 +1,48 @@
 package nttdata.grupouno.com.operations.controllers;
 
 import lombok.RequiredArgsConstructor;
-import nttdata.grupouno.com.operations.repositories.implementation.MasterAccountRepository;
-import nttdata.grupouno.com.operations.services.implementation.MasterAccountServices;
+import nttdata.grupouno.com.operations.models.TypeModel;
+import nttdata.grupouno.com.operations.services.IAccountClientService;
+import nttdata.grupouno.com.operations.services.IMasterAccountServices;
+import nttdata.grupouno.com.operations.services.ITypeAccountService;
+import reactor.core.publisher.Mono;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @ExtendWith(MockitoExtension.class)
 @RequiredArgsConstructor
 public class MasterAccountControllerTest {
-    @Mock
-    private MasterAccountRepository masterAccountRepository;
     @InjectMocks
-    private MasterAccountServices masterAccountServices;
+    private MasterAccountController masterAccountServices;
+    @Mock
+    private IMasterAccountServices accountServices;
+    @Mock
+    private ITypeAccountService typeAccountService;
+    @Mock
+    private IAccountClientService accountClientService;
+    @Autowired
+    private TypeModel typeModel;
     //@Autowired
     //private final WebClient webClient;
 
-    //@Test
-    //void findByClient(){
+    @BeforeEach
+    void init(){
+        typeModel = new TypeModel("AHO1", "Ahorro", "A", 2, 0.0, 1, 0, 0.00);
+        Mockito.when(typeAccountService.findById("AHO")).thenReturn(Mono.empty());
+        Mockito.when(typeAccountService.findById("AHO1")).thenReturn(Mono.just(typeModel));
+        
 
-    //}
+    }
+
+    @Test
+    void createAccountBank(){
+
+    }
 }
