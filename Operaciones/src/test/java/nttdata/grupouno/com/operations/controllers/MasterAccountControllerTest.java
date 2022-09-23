da204d1460d6aecb46ca25f3fa53614ebc203ec7
Francisco Frank CY <108429033+FranciscoCY@users.noreply.github.com>
2022-09-23 15:39:30 -0500

    Unit Test - mockito

--- Operaciones/src/test/java/nttdata/grupouno/com/operations/controllers/MasterAccountControllerTest.java
+++ Operaciones/src/test/java/nttdata/grupouno/com/operations/controllers/MasterAccountControllerTest.java
@@ -3,13 +3,10 @@ package nttdata.grupouno.com.operations.controllers;
 import lombok.RequiredArgsConstructor;
 import nttdata.grupouno.com.operations.repositories.implementation.MasterAccountRepository;
 import nttdata.grupouno.com.operations.services.implementation.MasterAccountServices;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @ExtendWith(MockitoExtension.class)
 @RequiredArgsConstructor
@@ -18,11 +15,11 @@ public class MasterAccountControllerTest {
     private MasterAccountRepository masterAccountRepository;
     @InjectMocks
     private MasterAccountServices masterAccountServices;
-    @Autowired
-    private final WebClient webClient;
+    //@Autowired
+    //private final WebClient webClient;
 
-    @Test
-    void findByClient(){
+    //@Test
+    //void findByClient(){
 
-    }
+    //}
 }
